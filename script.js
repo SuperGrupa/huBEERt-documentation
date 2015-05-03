644cdddac08b1023722835a21eb0a4295a93fccc
HFScofield <HFScofield@gmail.com>
2015-05-04 01:18:08 +0200

    More of prototype

--- script.js
+++ script.js
@@ -7,21 +7,6 @@ function toggle_visibility(id) {
 	}
 	else {
 		e.style.display = 'block';
-		b	.innerHTML = "Mniej opcji";
+		b.innerHTML = "Mniej opcji";
 	}
-}
-
-var counter = 1;
-var limit = 3;
-
-function next_beer(divName) {
-	if(counter < limit) {
-		var newdiv = document.createElement('div');
-		var nextbeer
-		newdiv.innerHTML = "<br><input type='text' class='form-control' placeholder='Szukaj według nazwy piwa'>";
-		document.getElementById(divName).appendChild(newdiv);
-		counter++;
-	}
-	if(counter == limit)
-		document.getElementById('newbeer').style.display = 'none';
 }
